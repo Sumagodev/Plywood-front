1d4e0c1527433fc53c77ab671dd40d6a72b4708d
2412pooja1 <pdalvi0203@gmail.com>
2024-09-10 19:17:52 +0530

    changes in UI

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,10 +1,10 @@
 // local urlyar
 // export const url = "http://localhost:3016";
 export const adminUrl = "https://admin.plywoodbazar.com";
-
+// 
 // production url
-// export const url = "https://api.plywoodbazar.com";
-export const url = "http://13.232.176.232/api";
+export const url = "https://api.plywoodbazar.com";
+// export const url = "http://13.232.176.232/api";
 
 export const generateImageUrl = (path) => {
   return `${url}/uploads/${path}`;
