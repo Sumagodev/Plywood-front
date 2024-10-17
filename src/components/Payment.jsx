4acd7ca3425093257ea667d2f707a97ef5f30a69
2412pooja1 <pdalvi0203@gmail.com>
2024-10-17 12:38:01 +0530

    After changes

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -14,58 +14,52 @@ function Payment() {
   const [effective_amount, seteffective_amount] = useState("");
   const [txn_uuid, settxn_uuid] = useState("");
   const [txOrderStatus, setTxOrderStatus] = useState(false);
+  const [errormsg, seterrormsg] = useState("")
 
-  useEffect(() => {
-    if (params.id) {
-      setOrderId(params.id)
-      setOrderIds(req.params?.orderId)
-      settxn_id(params?.txn_id)
-      settxn_uuid(params?.txn_uuid)
-      seteffective_amount(params?.effective_amount)
-      handlePhonePaymentCallback(params.id);
-
-    }
-  }, [orderIds])
 
   const searchParams = new URLSearchParams(location.search);
 
   const oserno = searchParams.get('orderId');
+  console.log('hgtdf', orderId)
+  useEffect(() => {
+    if (oserno === null) {
+      // setOrderId(params.id)
+      handlePhonePaymentCallback(params.id);
+
+    } else {
+      setOrderId(params.id);
+
+
+
+      handlePaymentHDFCCallback(oserno);
+    }
+  }, [params, location.search])
+
 
   const handlePhonePaymentCallback = async (id) => {
     try {
       setOrderStatus(1);
-      let { data: res } = await verifyPayment({ orderId: oserno });
+      // let { data: res } = await phonepePaymentStatusCheck(id);
+      // if (res.data) {
+      // } else {
+      //   setOrderStatus(2);
 
-      // Handle successful response
-      settxn_id(res.txn_id);
-      setOrderIds(res.orderId);
-      seteffective_amount(res.effective_amount);
-      settxn_uuid(res.txn_uuid);
-      setTxOrderStatus(res.status);
+      //   // setTimeout(()=>{
+      //   //   navigate('/')
+      //   // },3000)
 
+      // }
     } catch (error) {
-      // Check if error has a response with data
+      // errorToast(error)
+      // setOrderStatus(2);
 
-        const res = error.response.data;  // Extracting the response from the error object
-
-        // Handle the response even when it's an error
-        settxn_id(res.txn_id);
-        setOrderIds(res.orderId);
-        seteffective_amount(res.effective_amount);
-        settxn_uuid(res.txn_uuid);
-        setTxOrderStatus(res.status);
-
-        setOrderStatus(2);  // Set order status as failed
-     
-
-      // Redirect back to homepage after 3 seconds
-      setTimeout(() => {
-        navigate('/');
-      }, 120000);
+      // setTimeout(()=>{
+      //   navigate('/')
+      // },3000)
     }
-  };
+  }
 
-  
+
   const handlePaymentHDFCCallback = async (id) => {
     try {
       setOrderStatus(1);
@@ -77,21 +71,22 @@ function Payment() {
       seteffective_amount(res.effective_amount);
       settxn_uuid(res.txn_uuid);
       setTxOrderStatus(res.status);
+      seterrormsg(res.message)
 
     } catch (error) {
       // Check if error has a response with data
 
-        const res = error.response.data;  // Extracting the response from the error object
+      const res = error.response.data;  // Extracting the response from the error object
+      seterrormsg(error.response.data.message)
+      // Handle the response even when it's an error
+      settxn_id(res.txn_id);
+      setOrderIds(res.orderId);
+      seteffective_amount(res.effective_amount);
+      settxn_uuid(res.txn_uuid);
+      setTxOrderStatus(res.status);
 
-        // Handle the response even when it's an error
-        settxn_id(res.txn_id);
-        setOrderIds(res.orderId);
-        seteffective_amount(res.effective_amount);
-        settxn_uuid(res.txn_uuid);
-        setTxOrderStatus(res.status);
+      setOrderStatus(2);  // Set order status as failed
 
-        setOrderStatus(2);  // Set order status as failed
-     
 
       // Redirect back to homepage after 3 seconds
       setTimeout(() => {
@@ -100,85 +95,138 @@ function Payment() {
     }
   };
 
-  useEffect(() => {
-    if (params.id) {
-      // Set the orderId from URL params
-      setOrderId(params.id);
 
-
-
-      handlePhonePaymentCallback(params.id);
-    }
-  }, [params, location.search]);
   return (
+    <>
 
-    <main>
-      <section className="ptb-50 order-complete">
-        <div className="container">
-          <div className="row text-center my-5">
-            {/* Always show transaction details */}
-            <div className="col-12 col-md-8 col-lg-6 mx-auto">
-              {txOrderStatus === "CHARGED" ? (
-                <h2>
-                  Your Payment has been received{" "}
-                  <span className="emoji" role="img">
-                    🥳
-                  </span>
-                </h2>
-              ) : (
-                <h3>Payment Failed: Technical Issue Encountered during Subscription Process</h3>
-              )}
+      {oserno === null ?
+        <main>
+          <section className="ptb-50 order-complete">
+            <div className="container">
+              <div className="row text-center my-5">{
+                orderStatus == 1 && (<div className="col-12 col-md-8 col-lg-6 mx-auto">
+                  <h2>
+                    Your Payment has been received
+                    <span className="emoji" role="img">
+                      🥳
+                    </span>
+                  </h2>
+                  {/* <img src={images.party} alt="" className="mt-4 mb-5" /> */}
+                  <h5>Thank you for your Payment!</h5>
+                  <p className=" my-5">
+                    Your Payment ID is&nbsp;:&nbsp;
+                    <span className="green fw-semibold">{orderId}</span>
+                  </p>
+                  <p>
+                    You will receive a booking confirmation email on registerd Email Id.
+                  </p>
 
-              <h5>{txOrderStatus === "CHARGED" ? "Thank you for your Payment!" : "Please Contact Admin for Subscription Problem"}</h5>
+                </div>)
+              }
+                {orderStatus == 2 && (
+                  <div className="col-12 col-md-8 col-lg-6 mx-auto">
+                    <h3>Payment Failed: Technical Issue Encountered during Subscription Process</h3>
+                    {/* <img src={images.party} alt="" className="mt-4 mb-5" /> */}
+                    <h5>{`Please Contact Admin for Subscription Problem ${errormsg}`} </h5>
+                    <h5>{`${errormsg}`} </h5>
+                    <p className=" my-5">
+                      Your Payment ID is &nbsp;:&nbsp;
+                      <span className="green fw-semibold">{orderId}</span>
+                    </p>
 
-              {/* Transaction details table */}
-              <table className="table table-bordered mt-5">
-                <tbody>
-                  <tr>
-                    <td className="fw-semibold">Payment ID:</td>
-                    <td className="green fw-semibold">{orderId}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-semibold">Order ID:</td>
-                    <td className="green fw-semibold">{orderIds}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-semibold">Transaction ID:</td>
-                    <td className="green fw-semibold">{txn_id}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-semibold">Transaction UUID:</td>
-                    <td className="green fw-semibold">{txn_uuid}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-semibold">Amount:</td>
-                    <td className="green fw-semibold">{effective_amount}</td>
-                  </tr>
-                  <tr>
-                    <td className="fw-semibold">Order Status:</td>
-                    <td className="green fw-semibold">
-                      {txOrderStatus === "CHARGED" ? "Success" : "Failed"}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
 
-              {txOrderStatus === "CHARGED" && (
-                <p className="mt-4">
-                  You will receive a booking confirmation email on your registered email ID.
-                </p>
-              )}
-            </div>
+                  </div>)
+                }
 
-            {orderStatus === 0 && (
-              <div className="col-12 col-md-8 col-lg-6 mx-auto">
-                <h3>Please Wait</h3>
+                {orderStatus == 0 && (
+                  <div className="col-12 col-md-8 col-lg-6 mx-auto">
+                    <h3>Please Wait</h3>
+
+
+
+
+                  </div>)
+                }
               </div>
-            )}
-          </div>
-        </div>
-      </section>
-    </main>
+            </div>
+          </section>
+
+        </main>
+        :
+
+        <main>
+          <section className="ptb-50 order-complete">
+            <div className="container">
+              <div className="row text-center my-5">
+                {/* Always show transaction details */}
+                <div className="col-12 col-md-8 col-lg-6 mx-auto">
+                  {txOrderStatus === "CHARGED" ? (
+                    <h2>
+                      Your Payment has been received{" "}
+                      <span className="emoji" role="img">
+                        🥳
+                      </span>
+                    </h2>
+                  ) : (
+                    <h4>Payment Failed: Technical Issue Encountered during Subscription Process</h4>
+                  
+                  )}
+
+                  <h5>{txOrderStatus === "CHARGED" ? "Thank you for your Payment!" : `Please Contact Admin for Subscription Problem`}</h5>
+                  <h4  style={{color:'red'}}>{txOrderStatus === "CHARGED" ? "" : `${errormsg}`}</h4>
+
+
+                  {/* Transaction details table */}
+                  <table className="table table-bordered mt-5">
+                    <tbody>
+                      <tr>
+                        <td className="fw-semibold">Payment ID:</td>
+                        <td className="green fw-semibold">{orderId}</td>
+                      </tr>
+                      <tr>
+                        <td className="fw-semibold">Order ID:</td>
+                        <td className="green fw-semibold">{orderIds}</td>
+                      </tr>
+                      <tr>
+                        <td className="fw-semibold">Transaction ID:</td>
+                        <td className="green fw-semibold">{txn_id}</td>
+                      </tr>
+                      <tr>
+                        <td className="fw-semibold">Transaction UUID:</td>
+                        <td className="green fw-semibold">{txn_uuid}</td>
+                      </tr>
+                      <tr>
+                        <td className="fw-semibold">Amount:</td>
+                        <td className="green fw-semibold">{effective_amount}</td>
+                      </tr>
+                      <tr>
+                        <td className="fw-semibold">Order Status:</td>
+                        <td className="green fw-semibold">
+                          {txOrderStatus === "CHARGED" ? "Success" : "Failed"}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                  {txOrderStatus === "CHARGED" && (
+                    <p className="mt-4">
+                      You will receive a booking confirmation email on your registered email ID.
+                    </p>
+                  )}
+                </div>
+
+                {orderStatus === 0 && (
+                  <div className="col-12 col-md-8 col-lg-6 mx-auto">
+                    <h3>Please Wait</h3>
+                  </div>
+                )}
+              </div>
+            </div>
+          </section>
+        </main>
+      }
+
+    </>
   );
 }
 
